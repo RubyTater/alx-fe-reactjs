322ca0d371ecbd46ff9ada72ed5a591aef3d3eb1
Ruby Tater <rubytater10@gmail.com>
2025-07-11 18:43:50 +0000

    add header

---
+++ alx-react-app/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header () {
+    return (<header>
+        <h1>My Favorite Cities</h1>
+      </header>) ;
+      
+}
+
+
+function Subheader () {
+    return (<div>Subheader</div>)
+}
+
+export default Header
+export {Subheader}
