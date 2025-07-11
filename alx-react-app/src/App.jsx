69ae11ea02d7dc02707087cbcfff77125b09b42e
Ruby Tater <rubytater10@gmail.com>
2025-07-11 18:57:14 +0000

    props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,8 @@ function App() {
       <MainContent />
 
       <Footer />
+
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
