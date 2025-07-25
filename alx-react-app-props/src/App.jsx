b7b48f6662062b25430d44a8885de7ac310a386d
Ruby Tater <rubytater10@gmail.com>
2025-07-25 13:47:42 +0000

    fix breaking change after removing UserProfile

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
 import ProfilePage from './components/ProfilePage'
 import UserContext from './components/UserContext'
 
@@ -25,8 +24,6 @@ function App() {
       </UserContext.Provider>
 
       <Footer />
-
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
