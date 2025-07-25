4fbd74d09660427757a082cb249ff3d3c08f60ef
Ruby Tater <rubytater10@gmail.com>
2025-07-25 13:54:58 +0000

    include backgroundColor inline styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link, Outlet} from 'react-router-dom';
 function Navbar() {
     return (
         <>
-        <div style={{display: 'flex', justifyContent: "space-between", maxWidth: "780px"}}>
+        <div style={{display: 'flex', justifyContent: "space-between", maxWidth: "780px", backgroundColor: 'navy'}}>
             <Link to="/">Home</Link>
             <Link to="about">About Us</Link>
             <Link to="services">Services</Link>
