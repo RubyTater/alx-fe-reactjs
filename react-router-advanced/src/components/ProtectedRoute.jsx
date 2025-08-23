f871b201496820e0d5b93be3c6cca81dacecfdee
Ruby Tater <rubytater10@gmail.com>
2025-08-23 17:56:30 +0000

    change auth name

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,12 +2,12 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-const isAuthenticated = () => {
+const useAuth = () => {
   return localStorage.getItem("auth") === "true"; // simple mock login check
 };
 
 const ProtectedRoute = ({ children }) => {
-  if (!isAuthenticated()) {
+  if (!useAuth()) {
     return <Navigate to="/login" replace />;
   }
   return children;
